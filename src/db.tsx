52658c3406e73f2d18706a7daf0e1a0f1e28a5d4
Trung - FS - HCM <trung.nguyenviet@seedcom.vn>
2025-12-24 13:54:08 +0700

    up

--- src/db.tsx
+++ src/db.tsx
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import postgres from "postgres";
+const DATABASE_URL =
+    process.env.DATABASE_URL_POOL || process.env.DATABASE_URL
 
-if (!process.env.DATABASE_URL) {
+if (!DATABASE_URL) {
     throw new Error("DATABASE_URL environment variable is not set. Please create a .env file with your Neon database URL.");
 }
 
-export const sql = postgres(process.env.DATABASE_URL, {
+export const sql = postgres(DATABASE_URL, {
     ssl: "require",
     max: 5,
     idle_timeout: 30,
